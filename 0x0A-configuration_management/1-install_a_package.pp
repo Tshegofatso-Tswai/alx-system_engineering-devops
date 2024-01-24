4335c9606576510e51829043eb4d3a3e4b62ac9c
Tshegofatso Tswai <pholosotswai@gmail.com>
2024-01-23 16:50:32 -0800

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,6 @@
-package { 'flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
-package { 'Werkzeug':
-  ensure   => '2.2.2',
-  provider => 'pip3',
+  provider => 'pip3'
 }
