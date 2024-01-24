bdd923646c61424a2fdefe9fda2715cac0a76a65
Tshegofatso Tswai <pholosotswai@gmail.com>
2024-01-23 16:47:17 -0800

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
-# Installation of flask from pip3
+# Using Puppet, install flask from pip3
+# Requirements:
+  # Install flask
+  # Version must be 2.1.0
 
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+package {
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
