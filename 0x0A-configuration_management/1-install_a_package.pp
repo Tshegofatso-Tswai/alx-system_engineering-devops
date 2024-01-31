37bb7e4f044d0201e19e676287f254bef1f0fe2a
Tshegofatso Tswai <pholosotswai@gmail.com>
2024-01-30 16:00:19 -0800

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,5 @@
-# Install flask version 2.1.0 from pip3
-
-$packages = [ 'python3', 'python3-pip']
-
-# Install python3 and pip3 if not already installed
-package { $packages:
-  ensure => 'installed',
-}
-
-# Install flask version 2.1.0 with pip3
-package { 'Flask':
+# Install flask
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip']
+  provider => 'pip3'
 }
